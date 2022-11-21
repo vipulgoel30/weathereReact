9fcd55686036d8d365392b8d5c029c6741b9f8b0
vipulgoel30 <96120213+vipulgoel30@users.noreply.github.com>
2022-11-21 14:40:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   const changeInput = () => {
     const value = document.querySelector(".data").value;
     fetch(
-      `https://api.locationiq.com/v1/autocomplete?key=${process.env.locationIQ}&q=${value}&limit=5&dedupe=1`
+      `https://api.locationiq.com/v1/autocomplete?key=${process.env.REACT_APP_LOCATION}&q=${value}&limit=5&dedupe=1`
     )
       .then((temp) => {
         return temp.json();
