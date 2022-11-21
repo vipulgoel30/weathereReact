73dec033c681d035163f3a0e825d9233b82df5a5
vipulgoel30 <96120213+vipulgoel30@users.noreply.github.com>
2022-11-21 14:31:08 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   const changeInput = () => {
     const value = document.querySelector(".data").value;
     fetch(
-      `https://api.locationiq.com/v1/autocomplete?key=pk.df78536eeeba29510f84715f48d43834&q=${value}&limit=5&dedupe=1`
+      `https://api.locationiq.com/v1/autocomplete?key=${process.env.locationIQ}&q=${value}&limit=5&dedupe=1`
     )
       .then((temp) => {
         return temp.json();
